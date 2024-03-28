5c93e848cee1bfd6fcdb80bc5f3cc85e18fa2e9e
Jesse Crowe <jesse.crowe@mykenzie.snhu.edu>
2024-03-28 10:21:53 -0600

    search bar working on community page

--- client/src/components/books.jsx
+++ client/src/components/books.jsx
@@ -1,30 +1,57 @@
 
 import { Grid } from "@material-ui/core";
 import { useEffect, useState } from "react";
+import Fuse from 'fuse.js'
 
 const Books = () => {
-    const [books, setBooks] = useState(null);
+	const [books, setBooks] = useState(null);
+	const [query, setQuery] = useState('');
+	
+	useEffect(() => {
+		fetch("http://localhost:3001/api/books")
+			.then(response => response.json())
+			.then(data => setBooks(data))
+			.catch(error => console.log(error))
+	}, []);
 
-    useEffect(() => {
-        fetch("http://localhost:3001/api/books")
-            .then(response => response.json())
-            .then(data => setBooks(data))
-            .catch(error => console.log(error))
-    },[]);
+	
 
-// columnSpacing={{ xs: 2, sm: 4, md: 6 }}
-    return (
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-            {books && books.map((book) => (
-                <Grid item xs={2} md={4} key={book._id} >
-                    <item className='displayBook'>
-                        <img src={book.image_url}/>
-                        <p>{book.title}</p>
-                    </item>
-                </Grid>
-            ))}
-        </Grid>
-    );
+	const fuse = books ? new Fuse(books, {keys: ['title'], threshold: 0.1, }) : null;
+
+	
+	let bookResults = books;
+	if (fuse && query) {
+		const results = fuse.search(query)
+		bookResults = results.map(result => result.item)
+	}
+
+	const handleBookSearch = (e) => {
+		const { value} = event.target;
+		setQuery(value)
+	}
+	
+	// columnSpacing={{ xs: 2, sm: 4, md: 6 }}
+	return (
+		<>
+		   <input
+        type="text"
+        placeholder="Search Books"
+				value={query}
+				onChange={handleBookSearch}
+        style={{ float: "right" }}
+      ></input> 
+			<Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+				{books && bookResults.map((book) => (
+					<Grid item xs={2} md={4} key={book._id} >
+						<item className='displayBook'>
+							<img src={book.image_url} />
+							<p>{book.title}</p>
+						</item>
+					</Grid>
+				))}
+			</Grid>
+		</>
+	);
 };
 
 export default Books;
--- client/src/pages/CommunityPage.jsx
+++ client/src/pages/CommunityPage.jsx
@@ -2,36 +2,15 @@
 import React from "react";
 import Books from "../components/books";
 
-// const search = ({ searchBooks })
-//     const [bookList, setBookList] = useState('');
 
-// const handleSearch = () => {
-//     searchBooks(query)
-// }
 
 function CommunityPage() {
   return (
     <div className="community-page">
       <h2 className="availableBooks">Books Available</h2>
-      <input
-        type="text"
-        id="bookSearch"
-        placeholder="Search Books"
-        title="type in a book"
-        style={{ float: "right" }}
-      ></input> 
       <div>
       <Books/>
       </div>
-      {/* <div>
-            <input
-            type='text'
-            placeholder='Search Books'
-            value={query}
-            onChange={(e) => setBookList(e.target.value)}
-            />
-            <button onClick={handleSearch}>Search</button>
-        </div> */}
     </div>
   );
 }
